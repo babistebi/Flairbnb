ee133df88409d0959bd71f9d063807c9bfc4302d
babistebi <babistebi94@googlemail.com>
2023-08-13 14:01:01 +0200

    finalized the login_page.php

--- login_page.php
+++ login_page.php
@@ -1,15 +1,37 @@
 <?php
-
 //connect to database
-$db = mysqli_connect('localhost', 'root', '', 'flairbnb')
-or die('Error connecting to databse.');
+$conn = mysqli_connect('localhost', 'root', '', 'flairbnb') or die('Error connecting to database.');
+
+//user inputs
+
+// username OR email
+$login_user = mysqli_real_escape_string($conn, $_REQUEST["username"]);
+// Passwort 
+$login_password = mysqli_real_escape_string($conn, $_REQUEST['psw']); 
+// Query  to find user
+$query = "SELECT * FROM registered WHERE email = '$login_user' OR username = '$login_user'";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) === 1) {
+    $user_data = mysqli_fetch_assoc($result);
+    $hashed_password = $user_data['password'];
+
+    // Verwenden Sie password_verify(), um das eingegebene Passwort mit dem gehashten Passwort in der Datenbank zu überprüfen
+    if (password_verify($login_password, $hashed_password)) {
+        echo "Login successful! Redirecting...";
+        echo ' <script type="text/javascript">
+        	function redirect() {
+            setTimeout(function() {
+            window.location.href = "index.html";}, 3000);}
+       		window.onload = redirect;
+    		</script>';}
+    	else {
+        echo "Login failed. Invalid credentials.";
+    }
+} else {
+    // Benutzer nicht gefunden
+    echo "User not found.";
+}
 
 
-//get values
-$login_user = $_REQUEST['email || username'];
-
-$login_password = $_REQUEST['psw'];
-
-
-//Execute query
-$query_user = "SELECT "
+?>
